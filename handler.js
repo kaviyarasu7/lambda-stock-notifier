25ca1007af5d280c81fe5ee138c3cf9c50682a16
Kaviyarasu7 <arasu.kavi72001@gmail.com>
2018-04-04 12:22:39 +0000

    added a simple home page to subscribe notificationsy

--- handler.js
+++ handler.js
@@ -3,11 +3,10 @@ const {extractListingsFromHTML, sendPushNotification} = require('./helpers');
 var stocks = {};
 module.exports.stocknotifier = (event, context, callback) => {
      request.get('http://getquote.icicidirect.com/trading_stock_quote.aspx?Symbol=INDCOU')
-    .then(({data}) => {
-      stocks = extractListingsFromHTML(data);
-      return sendPushNotification(stocks);
-    }).then((res) => {
-         callback(null, {stocks});  
-    })
-    .catch(callback);
+    	.then(({data}) => {
+      		stocks = extractListingsFromHTML(data);
+      		return sendPushNotification(stocks);
+    	}).then((res) => {
+         	callback(null, {stocks});  
+    	}).catch(callback);
 };
