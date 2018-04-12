8ffd378e5f3213c8c3d484f2348bc8d0070753e0
Kaviyarasu7 <arasu.kavi72001@gmail.com>
2018-04-12 07:21:49 +0000

    added seprate folder for webapp

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ var stocksListData = {};
 var stocksListURL = 'https://script.google.com/macros/s/AKfycbwxBcQ-BlbpTP_hUzZXnXJvcNTRnf8Whyt8eKHE4Chtl1P4MsLr/exec';
 module.exports.stocknotifier = (event, context, callback) => {
     request.get(stocksListURL).then(response => {
+    		console.log(response.data.records.NSEStockCode);
   			return getAllStockData(response.data.records)
   	}).then( promises => {
   			return request.all(promises).then(request.spread((...args) => {
