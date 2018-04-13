c99cab3306936fdf17e05ec1e2b9a05616d7020e
Kaviyarasu7 <arasu.kavi72001@gmail.com>
2018-04-13 12:46:16 +0000

    stock list condition validation checked

--- handler.js
+++ handler.js
@@ -1,21 +1,22 @@
 const request = require('axios');
-const {extractListingsFromHTML, sendPushNotification, getAllStockData, getAllStockDataParse} = require('./helpers');
+const {extractListingsFromHTML, sendPushNotification, getAllStockData} = require('./helpers');
 var stocks = [];
 var stocksList = [];
+var stocksWatchList = [];
 var stocksListData = {};
 var stocksListURL = 'https://script.google.com/macros/s/AKfycbwxBcQ-BlbpTP_hUzZXnXJvcNTRnf8Whyt8eKHE4Chtl1P4MsLr/exec';
 module.exports.stocknotifier = (event, context, callback) => {
     request.get(stocksListURL).then(response => {
-    		console.log(response.data.records.NSEStockCode);
+        stocksWatchList = response.data.records;
   			return getAllStockData(response.data.records)
   	}).then( promises => {
   			return request.all(promises).then(request.spread((...args) => {
         		for (let i = 0; i < args.length; i++) {
-            		stocks[i] = extractListingsFromHTML(args[i].data);
+            		stocks[i] = extractListingsFromHTML(args[i].data,stocksWatchList[i]);
         		}
   			}))
     }).then( response => {
-    		callback(null,{stocks})
+    		callback(null,stocks)
   	}).catch(error => {
      			console.log(error);
   	});
--- helpers.js
+++ helpers.js
@@ -5,13 +5,23 @@ const _ = require('lodash');
 
 cheerioTableparser = require('cheerio-tableparser');
 
-function extractListingsFromHTML (html) {
+function extractListingsFromHTML (html,stockWatchList) {
   const $ = cheerio.load(html);
   cheerioTableparser($);
   htmlData = $("#NewHide table").parsetable(true, false, false);
-  var cleaned = _.each(htmlData, o => _.each(o, (v, k) => o[k] = v.trim()))
-  //const htmlData = $('#NewHide tr:nth-child(2) td:nth-child(2)').text().trim();  
-  return cleaned;
+  var cleaned = _.each(htmlData, o => _.each(o, (v, k) => o[k] = v.trim()));
+  var cleanedSpace = _.each(cleaned, o => _.each(o, (v, k) => o[k] = v.replace(/ /g,"_")));
+  var readableData = {"code":stockWatchList.NSEStockCode,"LAST_TRADE_PRICE_NSE":cleanedSpace[1][1],"LAST_TRADE_PRICE_BSE":cleanedSpace[2][1]};
+  
+  
+  var condition = parseFloat(cleanedSpace[1][1])+" "+stockWatchList.Condition+" "+parseFloat(stockWatchList.Value);
+  if (eval(condition)) {
+    sendPushNotification(condition);
+    console.log("pass",condition);
+  } else {
+    console.log("fail",condition);
+  }
+  return readableData;
 }
 
 function sendPushNotification(stocks)
@@ -36,19 +46,11 @@ function getAllStockData(stocksList)
   var promises = [];
   stocksList.forEach(function(singleElement){
     myUrl = singleElement.ICICILINK;
-    //console.log(myUrl)
     promises.push(request.get(myUrl))
   });
   return promises;
 }
 
-
-function getAllStockDataParse(promises)
-{
-
-}
-
-
 module.exports = {
-  extractListingsFromHTML, sendPushNotification, getAllStockData, getAllStockDataParse
+  extractListingsFromHTML, sendPushNotification, getAllStockData
 };
